b72367527ac00e3398e6d58fea43ed25526f6369
Arijit <arijitbairagya@yahoo.com>
2018-08-03 18:37:39 +0800

    excel split

---
+++ src/main/java/com/example/excel/ExcelSplitter.java
@@ -0,0 +1,195 @@
+package com.example.excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.util.Assert;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelSplitter {
+
+	private final String fileName;
+	private int maxRows = 1000; // default to thousand
+	private int wbCount = 1;
+	private int totalColumns = 0;
+	private List<String> headerCells = new ArrayList<String>();
+	
+	private static Logger LOGGER = LogManager.getLogger(ExcelSplitter.class);
+
+
+	public static void main(String... args) {
+		LOGGER.debug("Argument Length - {}", args.length);
+		if(args.length == 1) { // only file name
+			
+		}
+		else if(args.length == 2) { // file name and chunk size
+			
+		}
+		else {
+			LOGGER.debug("Invalud Arguments Specified. Please provide file path and chunk size.");
+			throw new AssertionError("Invalud Arguments Specified. Please provide file path and chunk size.");
+		}
+			
+		LOGGER.debug("File Name : {}", args[0]);
+		LOGGER.debug("Chunk size : {}", args[1]);
+		/* Pass the file path and number of rows per sheet to split */
+		new ExcelSplitter(String.valueOf(args[0]), Integer.parseInt(args[1]));
+	}
+
+	public ExcelSplitter(String fileName, final int maxRows) {
+
+		this.fileName = fileName;
+		this.maxRows = maxRows;
+
+		try {
+			/* Read in the original Excel file. */
+			XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(fileName));
+			XSSFSheet sheet = workbook.getSheetAt(0);
+
+			/* Only split if there are more rows than the desired amount. */
+			if (sheet.getPhysicalNumberOfRows() >= maxRows) {
+				splitWorkbook(workbook);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("COMPLETED...");
+	}
+
+
+	private List<SXSSFWorkbook> splitWorkbook(XSSFWorkbook workbook) {
+
+		SXSSFWorkbook newWb = new SXSSFWorkbook();
+		Sheet newSheet = newWb.createSheet();
+		
+
+		Row newRow;
+		int rowCount = 0;
+
+		XSSFSheet sheet = workbook.getSheetAt(0);
+
+		for (Row row : sheet) {
+
+			// set number of cells per row as it is there in header
+			if(row.getRowNum() == 0) {
+				System.out.println("Number of headers: "+ row.getPhysicalNumberOfCells());
+				row.cellIterator().forEachRemaining( cell -> headerCells.add(cell.getStringCellValue()));
+				System.out.println("Headers:" + headerCells);
+				totalColumns = row.getPhysicalNumberOfCells();
+			}
+
+			newRow = newSheet.createRow(rowCount++);
+
+			// if number of rows reach the limit create another work
+			if (rowCount == maxRows+1) {
+				// write the workbook 
+				writeWorkBooks(newWb);
+				System.out.println("Creating New Workbook");
+				newWb = new SXSSFWorkbook();
+				newSheet = newWb.createSheet();
+
+				// create header in the first row
+				copyHeader(newSheet);
+				rowCount = 1;
+			}
+
+			copyCellValuebyRow(newWb, newRow, row);
+		}
+
+		/* Only add the last workbook if it has content */
+		if (newWb.getSheetAt(0).getPhysicalNumberOfRows() > 0) {
+			writeWorkBooks(newWb);
+		}
+		return null;
+	}
+
+	private void copyHeader(Sheet sheet) {
+		int col =0;
+		Row headerRow = sheet.createRow(0);
+		for (String cellVal : headerCells) {
+			headerRow.createCell(col++).setCellValue(cellVal);
+		}
+	}
+
+	private void copyCellValuebyRow(SXSSFWorkbook newWb, Row newRow, Row row) {
+		Cell newCell;
+		for(int cellCount=0; cellCount < totalColumns; cellCount++) {
+			newCell = newRow.createCell(cellCount);
+			Cell cellVal = row.getCell(cellCount);
+			setValue(newCell, cellVal);
+
+			CellStyle newStyle = newWb.createCellStyle();
+			newCell.setCellStyle(newStyle);
+		}
+	}
+
+	/*
+	 * Grabbing cell contents can be tricky. We first need to determine what
+	 * type of cell it is.
+	 */
+	private void setValue(Cell newCell, Cell cell) {
+		if(cell != null) {
+			switch (cell.getCellType()) {
+			case Cell.CELL_TYPE_STRING: 
+				newCell.setCellValue(cell.getRichStringCellValue().getString());
+				break;
+			case Cell.CELL_TYPE_NUMERIC:
+				if (DateUtil.isCellDateFormatted(cell)) {
+					newCell.setCellValue(cell.getDateCellValue());
+				} else {
+					newCell.setCellValue(cell.getNumericCellValue());
+				}
+				break;
+			case Cell.CELL_TYPE_BOOLEAN:
+				newCell.setCellValue(cell.getBooleanCellValue());
+				break;
+			case Cell.CELL_TYPE_FORMULA:
+				newCell.setCellFormula(cell.getCellFormula());
+				break;
+			case Cell.CELL_TYPE_BLANK:
+				newCell.setCellValue("");
+				break;
+			default:
+				System.out.println("Could not determine cell type - " + cell.getCellType());
+				System.out.println("Could not determine cell value - " + cell.getStringCellValue());
+			}
+		}
+	}
+
+	/* Write all the workbooks to disk. */
+	private void writeWorkBooks(SXSSFWorkbook wb) {
+		FileOutputStream out = null;
+		try {
+			//            for (int i = 0; i < wbs.size(); i++) {
+			String newFileName = fileName.substring(0, fileName.length() - 5);
+			out = new FileOutputStream(new File(newFileName + "_" + (wbCount++) + ".xlsx"));
+			wb.write(out);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(out != null)
+				try {
+					out.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+	}
+
+}
